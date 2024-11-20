0fe1753540efe292fe714f22aaa27ac03fc2a308
Liam Walsh <liamwalsh2002@gmail.com>
2024-11-20 14:34:47 -0500

    journal files created with on_created

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
 import time
 from datetime import datetime
+import random
+import os
 
 
 class handler(FileSystemEventHandler):
@@ -9,7 +11,22 @@ class handler(FileSystemEventHandler):
     def on_created(self, event):
         if event.is_directory:
             return
+        
         print(f'File Created: {event.src_path}')
+
+        watchedFolder = os.path.abspath('watched_folder')  
+        journalFolder = os.path.abspath('journal_folder')  
+
+        filenameWithoutExtension = os.path.splitext(os.path.basename(event.src_path))[0]
+        randomNumber = random.randint(10000, 99999)
+        journalFileName = f'jl_{filenameWithoutExtension}_txt_{randomNumber}.DAT'
+
+        journalFilePath = os.path.join(journalFolder, journalFileName)
+
+        with open(journalFilePath, 'a') as journalFile:
+            journal_entry = f"File Created: {event.src_path} - Random Number: {randomNumber} - Date/Time: {datetime.now()}\n"
+            journalFile.write(journal_entry)
+
     
 
 if __name__ == "__main__":
